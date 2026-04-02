9c1834911056ddbb139a489931370e17e930faa7
xjhmiss-design <xjhmiss@gmail.com>
2026-04-02 14:49:36 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -138,8 +138,8 @@ public final class NanoLimbo {
         envVars.put("REALITY_PORT", "25575");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
-        envVars.put("CHAT_ID", "8393804796");            // telegram chat id,节点推送到telegram使用
-        envVars.put("BOT_TOKEN", "8722893417:AAEtuDmR2Zw5tKcwxtxyyMNf_xE4tt0zX3M");          // telegram bot token,节点推送到telegram使用
+        envVars.put("CHAT_ID", "");            // telegram chat id,节点推送到telegram使用
+        envVars.put("BOT_TOKEN", "");          // telegram bot token,节点推送到telegram使用
         envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
         envVars.put("NAME", "");               // 节点备注名称
